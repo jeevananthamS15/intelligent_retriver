f13f0d8bdb489857a3b0ea0046c4db427fb6fbfa
jeevananthamS15 <sjeevanantham159@gmail.com>
2025-08-08 19:22:34 +0530

    Update in port in  main.py

--- main.py
+++ main.py
@@ -4,22 +4,18 @@ from api.endpoints import router
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
 
-# Create FastAPI app
 app = FastAPI(title="Insurance Policy Q&A API")
 
-# Include your API routes
 app.include_router(router, prefix="/api/v1")
 
-# Root endpoint for health check
 @app.get("/")
 def home():
     return {"message": "Service is running "}
 
-# Entry point
 if __name__ == "__main__":
-    # Use dynamic port for Render, default to 8000 locally
+    # Render assigns a port via the PORT env variable
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
